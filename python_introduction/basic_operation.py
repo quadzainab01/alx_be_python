d0b5a6616548709e289ed6e0c5927413f5035c65
Zainab Quadri <quadz_d3a6qcz@GRATEFUL-HEART.S.A.W>
2025-05-20 20:06:07 +0100

    Add basic_operations.py for arithmetic task

---
+++ python_introduction/basic_operation.py
@@ -0,0 +1,19 @@
+#assign specific values to variables
+number1 = 10
+number2 = 5
+
+#sum of the variables
+sum = number1 + number2
+
+print(f"Addition of {number1} and {number2} is {sum}")
+
+#difference of the variables
+difference = number1 - number2
+
+print(f"Subtraction of {number1} from {number2} is {difference}")
+
+#product of the variables
+product = number1 * number2
+
+print(f"Multiplication of {number1} and {number2} is {product}")
+
