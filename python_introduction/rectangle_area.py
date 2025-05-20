1924627785432157deb02a0fe4b3be3c857dc5df
Zainab Quadri <quadz_d3a6qcz@GRATEFUL-HEART.S.A.W>
2025-05-20 21:07:45 +0100

    Add rectangle_area.py for the calculation of the area of an object

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,12 @@
+#Assign specific values to two distinct variables
+
+length = 10 #The length of the rectangle.
+
+width = 5 #The width of the rectangle.
+
+
+#The formula to calculate the area of an object
+area = length * width
+
+#Print out the result
+print(f"The area of the rectangle is: {area}")
