c7d1e18d125e6bb38d4dcbd2884d2d6b9b4e46f6
Zainab Quadri <quadz_d3a6qcz@GRATEFUL-HEART.S.A.W>
2025-05-20 20:55:33 +0100

    Add simple_interest.py for the calculation of interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+#Assign specific values to  distinct variables
+principal = 1000
+rate = 0.05
+time = 3
+
+#State the simple interest formula
+interest = principal * rate * time
+
+#Print the result
+print(f"The simple interest is: {interest}")
